4ad5610ea51f8651cd9578f9cec2be9c825c7326
charlene-johnson <charlene.j8234@gmail.com>
2020-08-25 06:00:06 -0400

    Started writing out the functions, nothing in the functions yet

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/config")
+
+function find() {
+
+}
+
+function findById(id) {
+
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
+}
