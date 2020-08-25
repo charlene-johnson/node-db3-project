0ca51705352e16b48b35b38191c96d340c94ce7e
charlene-johnson <charlene.j8234@gmail.com>
2020-08-25 07:14:42 -0400

    finished the first three get functions so now I can get schemes, schemes by id and steps, finishing the other three functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,15 +1,22 @@
 const db = require("../data/config")
 
 function find() {
-
+    return db("schemes") 
 }
 
 function findById(id) {
-
+    return db("schemes")
+    .where("id", id)
+    .first()
 }
 
 function findSteps(id) {
-
+    return db("steps")
+    .innerJoin("schemes", "schemes.id", "steps.scheme_id")
+    .where("steps.scheme_id", id)
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .orderBy("steps.step_number")
+    
 }
 
 function add(scheme) {
